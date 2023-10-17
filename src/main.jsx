0b8e4deff1155e66de04de35daa170f4b712d0f6
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-10-18 01:10:32 +0600

    All setup done successfully

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from './Components/MainLayout/MainLayout.jsx';
+import ErrorPage from './Components/ErrorPage/ErrorPage.jsx';
+import Home from './Components/Home/Home';
+import AddProduct from './Components/AddProduct/AddProduct';
+import MyCart from './Components/MyCart/MyCart';
+import Login from './Components/Login/Login';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/addProduct',
+        element: <AddProduct></AddProduct>
+      },
+      {
+        path: '/myCart',
+        element: <MyCart></MyCart>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
